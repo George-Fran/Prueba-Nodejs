5af99e41b04086786b704fe8a67cd22085e76cd8
unknown <francogutierrez121@gmail.com>
2022-03-29 18:06:45 -0500

    nodegaleria

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/galeria_tutorial', {
+mongoose.connect('mongodb+srv://juancastro:canserbero123@cluster0.hgltu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     useNewUrlParser: true,
 })
     .then(db => console.log(`DB is connected`))
